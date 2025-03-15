cf495f9da1573e341df368e7d606b09edbe04acc
unknown <ayan25005@gmail.com>
2025-03-15 10:51:24 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import GeminiBody from "@/components/GeminiBody";
+import Sidebar from "@/components/Sidebar";
+
+
+export default function Home() {
+  return (
+     <div className="flex contain">
+       <Sidebar/> 
+       <GeminiBody/>
+
+     </div>
+  );
+}
+  
